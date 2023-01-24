fa4b3ca136bef7ff97ecd25f0b2164ec9ede783c
Tolutoon <103889304+Tolutoon@users.noreply.github.com>
2023-01-24 20:34:20 +0100

    first task dccumentation

--- sort.h
+++ sort.h
@@ -23,3 +23,10 @@ typedef struct listint_s
 
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+
+
+/*Sorting algorithms*/
+void bubble_sort(int *array, size_t size);
+
+
+#endif
